11215100a305667ee163f16d135b737e26dbe0fc
Ricardo <32184012+velleras@users.noreply.github.com>
2022-01-09 18:19:25 -0300

    Accessment
    Java and JavaScript Accessment.

---
+++ Java/App.java
@@ -0,0 +1,11 @@
+
+public class App {
+
+	public static void main(String[] args) {
+
+		System.out.println("Name: Ricardo de Sousa");
+		System.out.println("Nationality: Brazil");
+		System.out.println("My biggest Dream: My big professional dream is to work in a multinational company, with a team of different nationalities. "
+				+ "I believe I will fulfill my dream if I join this company.");
+	}
+}
